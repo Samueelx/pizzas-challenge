5a35842f2039d749ee5f1367d1d04f9174864088
Samueelx <gsammygitau@gmail.com>
2023-03-24 20:29:20 +0300

    add validations.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :pizzas, through: :restaurant_pizzas
+
+    validates :name, :address, presence: true
 end
--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,7 @@
 class RestaurantPizza < ApplicationRecord
   belongs_to :restaurant
   belongs_to :pizza
+
+  validates :price, presence: true
+  validates :price, numericality: {in: 1..30}
 end
